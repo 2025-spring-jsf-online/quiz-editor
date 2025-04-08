4356f83f078a1363ca042079245e3a098b7a4c81
MNuenninghoff <mikenue@gmail.com>
2025-04-08 16:54:29 -0500

    Marked for delete working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { QuizService } from './quiz.service';
 interface QuizDisplay {
   quizName: string;
   quizQuestions: QuestionDisplay[];
+  markedForDelete: boolean;
 }
 
 interface QuestionDisplay {
@@ -33,6 +34,7 @@ export class AppComponent implements OnInit {
       , quizQuestions: x.questions.map((y: any) => ({
         questionName: y.name
       }))
+      , markedForDelete: false
     }));
 
     console.log(this.quizzes);
@@ -52,6 +54,7 @@ export class AppComponent implements OnInit {
     const newQuiz = {
       quizName: "Untitled Quiz"
       , quizQuestions: []
+      , markedForDelete: false
     };
 
     this.quizzes = [
