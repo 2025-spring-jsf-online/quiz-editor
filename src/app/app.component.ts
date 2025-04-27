ac92c4e063ba8887a29d678b03c0dee7802dbae8
Justin Gritton-Bell <jgrittonbell@madisoncollege.edu>
2025-04-26 22:49:04 -0500

    Marked for delete working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { QuizService } from './quiz.service';
 interface QuizDisplay {
   quizName: string;
   quizQuestions: QuestionDisplay[];
+  markedForDelete: boolean;
 }
 
 interface QuestionDisplay {
@@ -30,6 +31,7 @@ export class AppComponent implements OnInit {
       quizQuestions: x.questions.map((y: any) => ({
         questionName: y.name,
       })),
+      markedForDelete: false,
     }));
 
     console.log(this.quizzes);
@@ -49,6 +51,7 @@ export class AppComponent implements OnInit {
     const newQuiz = {
       quizName: ' Untitled Quiz',
       quizQuestions: [],
+      markedForDelete: false,
     };
 
     this.quizzes = [...this.quizzes, newQuiz];
