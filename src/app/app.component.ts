ab3f5064204c538abcc8f6f9a4ae063f78a39ece
Justin Gritton-Bell <jgrittonbell@madisoncollege.edu>
2025-04-26 19:05:33 -0500

    Added addNewQuiz Funtionality
    Use event binding to call addNewQuiz when user clicks "Add New Quiz" button.
    addNewQuiz creates a new quizzes array by spreading the existing quizzes and adding a new untitled quiz at the end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,4 +44,15 @@ export class AppComponent implements OnInit {
 
     console.log(this.selectedQuiz);
   };
+
+  addNewQuiz = () => {
+    const newQuiz = {
+      quizName: ' Untitled Quiz',
+      quizQuestions: [],
+    };
+
+    this.quizzes = [...this.quizzes, newQuiz];
+
+    this.selectedQuiz = newQuiz;
+  };
 }
