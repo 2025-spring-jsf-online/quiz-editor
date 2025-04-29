b9b353885819b1dfed50ce8cd433f2aaf5f91f7f
Justin Gritton-Bell <jgrittonbell@madisoncollege.edu>
2025-04-29 12:12:17 -0500

    Got details from left animation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService, QuizFromWeb } from './quiz.service';
-import { generate } from 'rxjs';
+
+import {
+  trigger,
+  transition,
+  animate,
+  keyframes,
+  style,
+} from '@angular/animations';
 
 interface QuizDisplay {
   quizName: string;
@@ -19,6 +26,46 @@ interface QuestionDisplay {
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.css',
+  animations: [
+    trigger('detailsFromLeft', [
+      transition('leftPosition => finalPosition', [
+        animate(
+          '300ms',
+          keyframes([
+            style({ marginLeft: '-30px', offset: 0.0 }),
+            style({ marginLeft: '-20px', offset: 0.25 }),
+            style({ marginLeft: '-10px', offset: 0.5 }),
+            style({ marginLeft: '-5px', offset: 0.75 }),
+            style({ marginLeft: '0px', offset: 1.0 }),
+          ])
+        ),
+      ]),
+    ]),
+    trigger('pulseSaveCancelButtons', [
+      transition('nothingToSave => somethingToSave', [
+        animate(
+          '400ms',
+          keyframes([
+            style({
+              transform: 'scale(1.0)',
+              'transform-origin': 'top left',
+              offset: 0.0,
+            }),
+            style({
+              transform: 'scale(1.2)',
+              'transform-origin': 'top left',
+              offset: 0.5,
+            }),
+            style({
+              transform: 'scale(1.0)',
+              'transform-origin': 'top left',
+              offset: 1.0,
+            }),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent implements OnInit {
   title = 'quiz-editor';
@@ -67,6 +114,8 @@ export class AppComponent implements OnInit {
     this.selectedQuiz = q;
 
     console.log(this.selectedQuiz);
+
+    this.detailsFromLeftAnimationState = 'finalPosition';
   };
 
   addNewQuiz = () => {
@@ -184,4 +233,10 @@ export class AppComponent implements OnInit {
   get editedQuizCount() {
     return this.getEditedQuizzes().length;
   }
+
+  detailsFromLeftAnimationState = 'leftPosition';
+
+  detailsFromLeftAnimationDone = () => {
+    this.detailsFromLeftAnimationState = 'leftPosition';
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -14,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     NgbModule,
     FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
