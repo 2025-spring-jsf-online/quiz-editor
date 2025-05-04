7ee498491c98e1029b47b94e2f893f2d86271f5d
Emily Gimm <egimm@madisoncollege.edu>
2025-05-04 13:30:20 -0500

    got details from left animation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService, QuizFromWeb } from './quiz.service';
 
+import {
+  trigger,
+  transition,
+  animate,
+  keyframes,
+  style,
+
+  
+} from '@angular/animations';
+
 interface QuizDisplay {
   quizName: string;
   quizQuestions: QuestionDisplay[];
@@ -18,6 +28,28 @@ interface QuestionDisplay {
   templateUrl: './app.component.html',
   standalone: false,
   styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('detailsFromLeft', [
+      transition('leftPosition => finalPosition', [
+        animate('300ms', keyframes([
+          style({ marginLeft: '-30px', offset: 0.0 }),
+          style({ marginLeft: '-20px', offset: 0.25 }),
+          style({ marginLeft: '-10px', offset: 0.5 }),
+          style({ marginLeft: '-5px', offset: 0.75 }),
+          style({ marginLeft: '0px', offset: 1.0 })
+        ]))
+      ]),
+    ]),
+    trigger('pulseSaveCancelButtons', [
+      transition('nothingToSave => somethingToSave', [
+        animate('400ms', keyframes([
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 0.0 }),
+          style({ transform: 'scale(1.2)', 'transform-origin': 'top left', offset: 0.5 }),
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 1.0 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
   title = 'quiz-editor';
@@ -62,7 +94,6 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.loadQuizzesFromCloud();
   }
-
   quizzes: QuizDisplay[] = [];
 
   selectedQuiz: QuizDisplay | undefined = undefined;
@@ -70,6 +101,7 @@ export class AppComponent implements OnInit {
   selectQuiz = (q: QuizDisplay) => {
     this.selectedQuiz = q;
     console.log(this.selectedQuiz);
+    this.detailsFromLeftAnimationState = "finalPosition";
   };
 
   addNewQuiz = () => {
@@ -194,5 +226,11 @@ export class AppComponent implements OnInit {
 
   get editedQuizCount() {
     return this.getEditedQuizzes().length;
+  }
+
+  detailsFromLeftAnimationState = "leftPosition";
+
+  detailsFromLeftAnimationDone = () => {
+    this.detailsFromLeftAnimationState = "leftPosition";
   }
 }
