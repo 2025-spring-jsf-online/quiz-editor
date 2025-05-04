ff6b04767d475f0d6811ca1eb7f0c5e96169b061
Emily Gimm <egimm@madisoncollege.edu>
2025-05-04 15:40:07 -0500

    got the save button working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { QuizService, QuizFromWeb } from './quiz.service';
+import { QuizService, QuizFromWeb, ShapeForSavingNewQuizzes, ShapeForSavingEditedQuizzes } from './quiz.service';
 
 import {
   trigger,
@@ -232,5 +232,32 @@ export class AppComponent implements OnInit {
 
   detailsFromLeftAnimationDone = () => {
     this.detailsFromLeftAnimationState = "leftPosition";
+  };
+
+  saveQuizzes = async ()=> {
+    try {
+      const newQuizzes: ShapeForSavingNewQuizzes[] = this.getAddedQuizzes().map(x => ({
+        quizName: x.quizName
+        , quizQuestions: x.quizQuestions.map(y => y.questionName) 
+      }));
+
+
+      const editedQuizzes: ShapeForSavingEditedQuizzes[] = this.getEditedQuizzes().map(x => ({
+        quiz: x.quizName
+        , questions: x.quizQuestions.map(y => ({
+          question: y.questionName
+        }))
+      }));
+
+      const numberOfUpdatedQuizzes = await this.quizSvc.saveQuizzes(
+        editedQuizzes
+        , newQuizzes
+    );
+
+    console.log("numberOfUpdatedQuizzes", numberOfUpdatedQuizzes);
+    }
+    catch(err) {
+      console.error(err);
+    }
   }
 }
