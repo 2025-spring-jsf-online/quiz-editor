ccd672495a3d982b42a50d3c785f3cd968cb9aee
layanrushdi <lusmani@madisoncollege.edu>
2025-05-05 17:57:41 -0500

    defining the shape, and shaping the data from the service....
    Next, lets bind and show the quizzess

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from './quiz.service';
+
+interface QuizDisplay {
+  quizName: string;
+  quizQuestions: QuestionDisplay[];
+}
+
+interface QuestionDisplay {
+  questionName: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // <-- fixed here
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'quiz-editor';
+  quizzes: QuizDisplay[] = [];
+
+  constructor(public quizSvc: QuizService) {}
+
+  ngOnInit() {
+    const quizzes = this.quizSvc.loadQuizzes();
+    console.log(quizzes);
+
+    this.quizzes = quizzes.map((x: any) => ({
+      quizName: x.name,
+      quizQuestions: x.questions.map((y: any) => ({
+        questionName: y.name
+      }))
+    }));
+
+    console.log(this.quizzes);
+  }
 }
